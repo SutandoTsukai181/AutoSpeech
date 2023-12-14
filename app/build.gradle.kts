916fbf7cd2078598538d26d47849d55e6cf9b787
Flockshot <m.somaan@gmail.com>
2023-12-14 19:34:57 +0200

    Translate UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.mlkit:language-id-common:16.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -76,4 +77,5 @@ dependencies {
     implementation("com.google.cloud:google-cloud-speech:4.26.0")
     implementation("com.google.cloud:google-cloud-texttospeech:2.32.0")
     annotationProcessor("com.google.cloud:google-cloud-translate:2.31.0")
+    implementation("com.google.mlkit:language-id:17.0.4")
 }
---
+++ app/src/main/java/com/cng495/autospeech/TranslateActivity.java
@@ -0,0 +1,52 @@
+package com.cng495.autospeech;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.nl.languageid.LanguageIdentification;
+import com.google.mlkit.nl.languageid.LanguageIdentifier;
+
+import javax.annotation.Nullable;
+
+public class TranslateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+    }
+
+    private void getTextLanguage(String text)
+    {
+        LanguageIdentifier languageIdentifier = LanguageIdentification.getClient();
+
+        languageIdentifier.identifyLanguage(text)
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@Nullable String languageCode) {
+                                if (languageCode.equals("und"))
+                                {
+                                    Log.i("Test", "Can't identify language.");
+                                }
+                                else
+                                {
+                                    Log.i("Test", "Language: " + languageCode);
+                                }
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Model couldn’t be loaded or other internal error.
+                                // ...
+                            }
+                        });
+    }
+}
